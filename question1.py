fca01a8da20f61c0a300ea2f5045735eb8a1cbf6
corrosif <corentin.leleuxhe@gmail.com>
2023-12-16 20:25:22 +0100

    affichage mouvement brownien

--- question1.py
+++ question1.py
@@ -4,8 +4,11 @@ import matplotlib.pyplot as plt
 
 
 def mouvement_brownien(T,Nmc):
-	W=[0]
+	W,t=[0],[0]
 	for i in range(Nmc):
+		t.append(t[i]+T/Nmc)
 		W.append(W[i]+np.sqrt(T/Nmc)*np.random.normal())
-	return W
-print(mouvement_brownien(2,100000))
+	return t,W
+t,W=mouvement_brownien(2,1000000)
+plt.plot(t,W)
+plt.show()
