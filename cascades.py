65b7d4800576a6d88cecae3e646c852cffc43fb9
corrosif <corentin.leleuxhe@gmail.com>
2023-12-19 20:30:08 +0100

    implementation methode cascades

--- cascades.py
+++ cascades.py
@@ -4,31 +4,29 @@ import random as rd
 
 import fluctuation as fl #biblio perso
 
-
 N=5
-
-condition_intiale = [10, 12, 15, 18, 20]  # liste capital initial
+condition_intiale = [15]*N  # liste capital initial
 A = fl.multi_marche(condition_intiale) #je recupere la matrice de fluctuation du capital des banques
 X_T=[A[i][-1] for i in range(len(A))] #je recupere la matrice du capital a la maturité
-W_T=[18,8,22,56,8]
-C=[10,10,10,10,10]  #matrice des seuils critiques
+W_T=X_T
+C=[10]*N  #matrice des seuils critiques
 
 E=[
-	[0,3,0,0,6],
+	[0,3,0,0,1],
 	[3,0,0,0,0],
 	[3,3,0,0,0],
 	[2,2,2,0,2],
 	[0,2,3,3,0],
 ] #matrice des etats du réseau 
 
-
+#E*[1,1,1,1,1]=[9,3,6,8,8]
 
 #je pars du principe que R est fixé a R=0.2 pour implementer l'algo
-N=5
 
-D=[]
-D.append([i for i in range(N) if W_T[i]<=C[i]])
-D.append(D[-1]+[i for i in range(N) if i not in D[0]])
-
-
-print(D[0],D[1])
+def cascade(k):
+	N=5
+	R=0.4
+	if k==0:
+		return [i for i in range(N) if W_T[i]<=C[i]]
+	else:
+		return cascade(k-1)+[j for j in range(N) if j not in cascade(k-1) and W_T[j]-(1-R)*sum(E[j])<=C[j]]
