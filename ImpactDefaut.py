a9ec858823800e8e50f6a6154777540b605879c8
corrosif <corentin.leleuxhe@gmail.com>
2024-04-10 10:35:45 +0200

    ajout ImpactDefaut (il devient deter)

--- ImpactDefaut.py
+++ ImpactDefaut.py
@@ -13,37 +13,31 @@ X_start=dm.X_start
 D_sol_start=[i for i in range(N)]
 D_T_start=[]
 Nmc2=10000
-def X_T(X_init):	#on part de la matrice des conditions initales puis on les fait maturer via les marches multiple de fluctuation.py
-	X_T_multi=fl.multi_marche(X_start)
-	X_T=[]
-	for i in range(N):
-		X_T.append(X_T_multi[i][-1])
-	return X_T
-#print(X_T(X_start))
+
 def Impact(X_init)->int: #calcul le cout financier de la chute du systeme en inpout je prends X_start
-	X_T_resultat=X_T(X_init)
 	I,L=0,0
-	X_j=copy.deepcopy(X_T_resultat)
-	X_1,D_sol_1,D_T_1,q=dm.Chute_Domino(X_T_resultat,D_sol_start,D_T_start)
+	X_j=copy.deepcopy(X_init)
+	X_1,D_sol_1,D_T_1,q=dm.Chute_Domino(X_init,D_sol_start,D_T_start)
 	for j in D_T_1:
 		I+=X_j[j]
 		for p in D_sol_1:
 			L+=(1-R)*E[p][j]
 	return I+L
-def IndexRisqueSys(X_start): #calcul l'index du risque systemique
+#print(Impact(X_start))
+def IndexRisqueSys(X_init): #calcul l'index du risque systemique
 	last_value=[]
 	for i in range(Nmc2):
-		last_value.append(Impact(X_start))
+		last_value.append(Impact(X_init))
 	esperance=np.mean(last_value)
 	return esperance
 #print(IndexRisqueSys(X_start))
 
-def I_S(n): #permet d'obtenir n valeur d'impact
+def I_S(n,X_init): #permet d'obtenir n valeur d'impact
 	I_S=[]
 	for i in range(n):
-		I_S.append(Impact(X_start))
+		I_S.append(Impact(X_init))
 	return I_S	
-print(I_S(10000))
+#print(I_S(500,X_start))
 
 	
 '''
@@ -53,6 +47,14 @@ def affichage(n): #affiche le cout de la chute
 		X_T=[A[i][-1] for i in range(len(A))]
 		print(Impact(X_T))
 	return 1
+	
+def X_T(X_init):	#on part de la matrice des conditions initales puis on les fait maturer via les marches multiple de fluctuation.py
+	X_T_multi=fl.multi_marche(X_start)
+	X_T=[]
+	for i in range(N):
+		X_T.append(X_T_multi[i][-1])
+	return X_T
+#print(X_T(X_start))
 
 '''
 
--- domino.py
+++ domino.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import copy
-N=5 #nb de banque
+N=10 #nb de banque
 R=0.05 #taux de recouvrement
 C=[10]*N  #matrice des seuils critiques
-X_start=[15,9,8,18,12]#[12,9,11,13,12]#[14,15,11,9,12,12,13,14,15,12]#[20,9,20,9,25,20,12,14,15,13]#[14,9,8,13,15]#
+X_start=[14,15,11,9,12,12,13,14,15,12]#[20,9,20,9,25,20,12,14,15,13]#[14,9,8,13,15]#[15,9,8,18,12]#[12,9,11,13,12]#
 D_sol_start=[i for i in range(N)]
 D_T_start=[]
  #matrice des etats du réseau
-'''
+
 E=[
 	[0,3,0,0,6,0,3,2,4,0],
 	[3,0,0,0,0,6,5,0,4,2],
@@ -28,6 +28,7 @@ E=[
 	[2,2,2,0,2],
 	[0,2,3,3,0],
 ]
+'''
 def Domino(X_T:list,D_sol:list,D_T:list)->list:
 	D=copy.deepcopy(D_sol)
 	D_faillite=copy.deepcopy(D_T)
@@ -44,6 +45,7 @@ def Domino(X_T:list,D_sol:list,D_T:list)->list:
 			somme+=E[i][j]	#on calcul a chaque fois l'impact de la faillite a chaque banque en defaut
 		X_T[i]-=(1-R)*somme  #on fait perdre le capital aux banques restantes 
 	return [X_T,D_sol,D_T]
+	
 def Chute_Domino(X_T:list,D_sol:list,D_T:list)->list: #a partir du jeux de base j'ai besoin de connaitre le nombre d'etape avant la faillite, stabilite du systeme
 	test=False
 	q=-1
@@ -55,6 +57,6 @@ def Chute_Domino(X_T:list,D_sol:list,D_T:list)->list: #a partir du jeux de base 
 		if len(D_sol_1)==0 or D_sol_1==X:
 			test=True
 	return X_1,D_sol_1,D_T_1,q
-
+#print(Chute_Domino(X_start,D_sol_start,D_T_start))
 	
 
